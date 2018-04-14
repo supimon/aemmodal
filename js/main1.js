901d93e96890b66b3bea6eaf936fd488e97a3dde
Supi <supimon@gmail.com>
2018-04-14 15:39:16 +0530

    got the gallery implemented

---
+++ js/main1.js
@@ -0,0 +1,91 @@
+jQuery(document).ready(function($){
+
+	//cache some jQuery objects
+	var modalTrigger = $('.cd-modal-trigger'),
+		transitionLayer = $('.cd-transition-layer'),
+		transitionBackground = transitionLayer.children(),
+		modalWindow = $('.cd-modal');
+
+	var frameProportion = 1.78, //png frame aspect ratio
+		frames = transitionLayer.data('frame'), //number of png frames
+		resize = false;
+
+	//set transitionBackground dimensions
+	setLayerDimensions();
+	$(window).on('resize', function(){
+		if( !resize ) {
+			resize = true;
+			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+		}
+	});
+
+	//open modal window
+	modalTrigger.on('click', function(event){	
+		event.preventDefault();
+		var modalId = $(event.target).attr('href');
+		transitionLayer.addClass('visible opening');
+		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 800;
+		setTimeout(function(){
+			modalWindow.filter(modalId).addClass('visible');
+			transitionLayer.removeClass('opening');
+		}, delay);
+	});
+
+	//close modal window
+	modalWindow.on('click', '.modal-close', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('closing');
+		modalWindow.removeClass('visible');
+		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+			transitionLayer.removeClass('closing opening visible');
+			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		});
+	});
+
+	function setLayerDimensions() {
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			layerHeight, layerWidth;
+
+		if( windowWidth/windowHeight > frameProportion ) {
+			layerWidth = windowWidth;
+			layerHeight = layerWidth/frameProportion;
+		} else {
+			layerHeight = windowHeight*1.2;
+			layerWidth = layerHeight*frameProportion;
+		}
+
+		transitionBackground.css({
+			'width': layerWidth*frames+'px',
+			'height': layerHeight+'px',
+		});
+
+		resize = false;
+	}
+
+  $('#download-btn').click(function(e) {
+    e.preventDefault();
+    $('.thank-you').hide();
+    if($('.input-holder').hasClass('expanded')){
+      $('#download-btn').text('Sending...');
+      // replace setTimeout with the AJAX call
+      setTimeout(function(){
+        $('#download-btn').text('Request Download');
+        $('.thank-you').fadeIn('fast').delay(3000).fadeOut('fast');
+      }, 2000);
+      $('.input-holder').slideUp(500, function(){
+        $(this).removeClass('expanded');
+        $('.modal-form input').val("");
+      });
+    }
+    else{
+      $('.input-holder').slideDown(500, function(){
+        $('#name_field').focus();
+        $(this).addClass('expanded');
+      });
+    }
+  });
+
+  $(".my-slider").as();
+
+});
