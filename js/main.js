b165b309464ac1b2f883d21641f7679c4dbd7625
Supi <supimon@gmail.com>
2018-04-14 13:22:24 +0530

    the modal is now responsive. Gallery remains

--- js/main.js
+++ js/main.js
@@ -63,36 +63,27 @@ jQuery(document).ready(function($){
 		resize = false;
 	}
 
-
-	$('#thumbnail li').click(function(){
-		var thisIndex = $(this).index()
-
-		if(thisIndex < $('#thumbnail li.active').index()){
-			prevImage(thisIndex, $(this).parents("#thumbnail").prev("#image-slider"));
-		}else if(thisIndex > $('#thumbnail li.active').index()){
-			nextImage(thisIndex, $(this).parents("#thumbnail").prev("#image-slider"));
-		}
-
-		$('#thumbnail li.active').removeClass('active');
-		$(this).addClass('active');
-
-	});
-
-  var width = $('#image-slider').width();
-
-  function nextImage(newIndex, parent){
-    parent.find('li').eq(newIndex).addClass('next-img').css('left', width).animate({left: 0},600);
-    parent.find('li.active-img').removeClass('active-img').css('left', '0').animate({left: -width},600);
-    parent.find('li.next-img').attr('class', 'active-img');
-  }
-  function prevImage(newIndex, parent){
-    parent.find('li').eq(newIndex).addClass('next-img').css('left', -width).animate({left: 0},600);
-    parent.find('li.active-img').removeClass('active-img').css('left', '0').animate({left: width},600);
-    parent.find('li.next-img').attr('class', 'active-img');
-  }
-
-	/* Thumbails */
-  var ThumbailsWidth = ($('#image-slider').width() - 18.5)/7;
-  $('#thumbnail li').find('img').css('width', ThumbailsWidth);
+  $('#download-btn').click(function(e) {
+    e.preventDefault();
+    $('.thank-you').hide();
+    if($('.input-holder').hasClass('expanded')){
+      $('#download-btn').text('Sending...');
+      // replace setTimeout with the AJAX call
+      setTimeout(function(){
+        $('#download-btn').text('Request Download');
+        $('.thank-you').fadeIn('fast').delay(3000).fadeOut('fast');
+      }, 2000);
+      $('.input-holder').slideUp(500, function(){
+        $(this).removeClass('expanded');
+        $('.modal-form input').val("");
+      });
+    }
+    else{
+      $('.input-holder').slideDown(500, function(){
+        $('#name_field').focus();
+        $(this).addClass('expanded');
+      });
+    }
+  });
 
 });
